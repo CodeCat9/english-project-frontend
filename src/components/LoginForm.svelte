f8195e2d3f7b90f2c79228101fac11e44ceff18a
Sakido9 <aesfirsthunter@hotmail.com>
2022-10-02 17:38:15 +0300

    Auth page front end functionality

--- src/components/LoginForm.svelte
+++ src/components/LoginForm.svelte
@@ -1,21 +1,54 @@
+<script>
+  import axios from 'axios';
+
+  let email = "";
+  let password = "";
+  let err = "";
+
+  function submitLogin(){
+    axios.post("https://english-project-364018.ey.r.appspot.com/api/auth/local", {
+      identifier: email,
+      password: password
+    }).then(response => {
+      console.log(response)
+    }).catch(error => {
+      if(error.response){
+        console.log(error)
+        err = error.response.data.error.details.errors[0].message;
+        err = err.charAt(0).toUpperCase() + err.slice(1) + "!"
+      }
+    })
+  }
+
+</script>
+
+
 <div>
-  <form action="/action_page.php">
+  <form on:submit|preventDefault={submitLogin}>
     <label for="email">Email</label>
-    <input type="text" id="email" name="firstname" placeholder="Your Email" />
+    <input type="text" id="email" name="firstname" placeholder="Email" bind:value={email} />
 
     <label for="password">Password</label>
-    <input
-      type="text"
+    <input 
+      bind:value={password}
+      type="password"
       id="password"
       name="password"
-      placeholder="Your password"
+      placeholder="Password"
     />
+
+    {#if err.length > 0}
+      <section class="error">{err}</section>
+    {/if}
 
     <input type="submit" value="Login" />
   </form>
 </div>
 
 <style>
+  .error{
+      color: crimson;
+    }
   input[type="text"]{
     width: 100%;
     padding: 1vw 2vw;
@@ -27,6 +60,17 @@
     border-radius: 4px;
     box-sizing: border-box;
   }
+  input[type="password"]{
+      width: 100%;
+      padding: 1vw 2vw;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      background-color:  #2f3b5c;
+      color: #dedede;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
   ::placeholder{
     color: #dedede;
   }
--- src/components/RegisterForm.svelte
+++ src/components/RegisterForm.svelte
@@ -1,24 +1,104 @@
+<script>
+  import axios from "axios";
+
+
+
+  let username = "";
+  let email = "";
+  let password = "";
+  let passwordConfirm = "";
+  let isEqual = true;
+
+  let err = "";
+  let validity = "";
+  let emailValidity = "";
+
+  const validateEmail = () => {
+    return email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+  function checkEqual(){
+    isEqual = password === passwordConfirm;
+    if(isEqual){
+      validity = "✓"
+    } else {
+      validity = "✖"
+    }
+  }
+
+  function isEmailValid(){
+    if(validateEmail()){
+      emailValidity = "✓"
+    } else {
+      emailValidity = "✖"
+    }
+  }
+
+  function submitRegister(){
+    axios.post("https://english-project-364018.ey.r.appspot.com/api/auth/local/register", {
+      username: username,
+      password: password,
+      email: email
+    }).then(res => {
+      console.log(res.data.jwt)
+    }).catch((error) =>{
+      if(error.response){
+        //console.log(error.response.data)
+        err = error.response.data.error.details.errors[0].message
+        err = err.charAt(0).toUpperCase() + err.slice(1) + "!"
+        console.log(error)
+      }
+    })
+  }
+
+</script>
+
+
 <div>
-    <form action="/action_page.php">
+    <form on:submit|preventDefault={submitRegister}>
         <label for="username">Username</label>
-      <input type="text" id="username" name="username" placeholder="Username" />
+      <input bind:value={username} type="text" id="username" name="username" placeholder="Username" />
       
-      <label for="email">Email</label>
-      <input type="text" id="email" name="email" placeholder="Your Email" />
+      <label for="email">Email {emailValidity}</label>
+      <input on:change={isEmailValid} bind:value={email} type="text" id="email" name="email" placeholder="Email" />
   
       <label for="password">Password</label>
       <input
-        type="text"
+        bind:value={password}
+        on:change={checkEqual}
+        type="password"
         id="password"
         name="password"
-        placeholder="Your password"
+        placeholder="Password"
       />
-  
+      
+      <label for="password">Confirm Password {validity}</label>
+      <input
+        bind:value={passwordConfirm}
+        on:change={checkEqual}
+        type="password"
+        id="confirm"
+        name="confirm"
+        placeholder="Confirm Password"
+      />
+
+      {#if err.length > 0}
+        <section class="error">{err}</section>
+      {/if}
+      
+
       <input type="submit" value="Register" />
     </form>
   </div>
   
   <style>
+    .error{
+      color: crimson;
+    }
     input[type="text"]{
       width: 100%;
       padding: 1vw 2vw;
@@ -30,6 +110,19 @@
       border-radius: 4px;
       box-sizing: border-box;
     }
+
+    input[type="password"]{
+      width: 100%;
+      padding: 1vw 2vw;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      background-color:  #2f3b5c;
+      color: #dedede;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+
     ::placeholder{
       color: #dedede;
     }
